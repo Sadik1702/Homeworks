6efb2b6b38d5455276f0440ee19bcd4441e0e06c
Sadık <sadiksezgin1702@hotmail.com>
2021-01-26 15:32:59 +0300

    Add project files.

---
+++ MyList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Homeworks
+{
+    class MyList
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> kitaplar = new MyDictionary<string>();
+            kitaplar.Add("Beyaz Diş");
+            kitaplar.Add("Beyaz Diş");
+            kitaplar.Add("Beyaz Diş");
+            kitaplar.Add("Beyaz Diş");
+            kitaplar.Add("Beyaz Diş");
+            Console.WriteLine(kitaplar.Count);
+        }
+    }
+
+    class MyDictionary<T>
+    {
+        T[] _array;
+        T[] _tempArray;
+        public MyDictionary()
+        {
+            _array = new T[0];
+        }
+        public void Add(T item)
+        {
+            _tempArray = _array;
+            _array = new T[_array.Length + 1];
+
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+
+            _array[_array.Length - 1] = item;
+        }
+
+        public int Count
+        {
+            get { return _array.Length; }  
+        }
+
+    }
+}
